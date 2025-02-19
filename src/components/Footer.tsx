2d2f2071b8706eedcd06c42cf1ec404a08dc4256
Kevin Olson <59523298+kevinolson1836@users.noreply.github.com>
2025-02-19 08:56:09 -0600

    Update Footer.tsx - cf analtics

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,57 +1,54 @@
 "use client";
 
 import { renderContent } from "@/app/resources";
-import { Flex, IconButton, SmartLink, Text } from "@/once-ui/components"
+import { Flex, IconButton, SmartLink, Text } from "@/once-ui/components";
 import { useTranslations } from "next-intl";
-import React, { useEffect } from "react";  // <-- Import useEffect from 
-// import { person, social } from '@/app/resources'
+import React, { useEffect } from "react";
 import ReactGA from "react-ga4";
-const TRACKING_ID = "G-RYLPF4LQNV";
 
+const TRACKING_ID = "G-RYLPF4LQNV";
 
 export const Footer = () => {
     const currentYear = new Date().getFullYear();
-
     const t = useTranslations();
     const { person, social } = renderContent(t);
 
     useEffect(() => {
-          ReactGA.initialize(TRACKING_ID);
-          let rawLocation = window.location.pathname
-          const parsedLocation = rawLocation.replace(/\//g, ''); // removes all '/'
-          ReactGA.send({ hitType: "pageview", page: parsedLocation});
-          console.log(parsedLocation);
-       }, []);
+        // Google Analytics
+        ReactGA.initialize(TRACKING_ID);
+        let rawLocation = window.location.pathname;
+        const parsedLocation = rawLocation.replace(/\//g, '');
+        ReactGA.send({ hitType: "pageview", page: parsedLocation });
+        console.log(parsedLocation);
+
+        // Cloudflare Web Analytics
+        const script = document.createElement("script");
+        script.src = "https://static.cloudflareinsights.com/beacon.min.js";
+        script.defer = true;
+        script.setAttribute("data-cf-beacon", '{"token": "ac87948dfd534a6ab0b6279f70dc03d8"}');
+        document.body.appendChild(script);
+
+        console.log("Cloudflare Analytics script added");
+
+    }, []);
 
     return (
-        
-        <Flex
-            as="footer"
-            position="relative"
-            fillWidth padding="8"
-            justifyContent="center">
-
-          
+        <Flex as="footer" position="relative" fillWidth padding="8" justifyContent="center">
             <Flex
                 fillWidth maxWidth="m" paddingY="8" paddingX="16"
-                justifyContent="space-between" alignItems="center">
-                <Text
-                    variant="body-default-s"
-                    onBackground="neutral-strong">
-                    <Text
-                        onBackground="neutral-weak">
-                        © {currentYear} /
-                    </Text>
-                    <Text paddingX="4">
-                        {person.name}
-                    </Text>
+                justifyContent="space-between" alignItems="center"
+            >
+                <Text variant="body-default-s" onBackground="neutral-strong">
+                    <Text onBackground="neutral-weak">© {currentYear} /</Text>
+                    <Text paddingX="4">{person.name}</Text>
                     <Text onBackground="neutral-weak">
-                        {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-                        / Build your portfolio with <SmartLink style={{marginLeft: '-0.125rem'}} href="https://once-ui.com/templates/magic-portfolio">Once UI</SmartLink>
+                        / Build your portfolio with 
+                        <SmartLink style={{ marginLeft: '-0.125rem' }} href="https://once-ui.com/templates/magic-portfolio">
+                            Once UI
+                        </SmartLink>
                     </Text>
                 </Text>
-                <Flex
-                    gap="16">
+                <Flex gap="16">
                     {social.map((item) => (
                         item.link && (
                             <IconButton
@@ -60,13 +57,12 @@ export const Footer = () => {
                                 icon={item.icon}
                                 tooltip={item.name}
                                 size="s"
-                                variant="ghost"/>
+                                variant="ghost"
+                            />
                         )
                     ))}
                 </Flex>
-                
             </Flex>
-            
         </Flex>
-    )
-}
+    );
+};
